b856bcca4f22532f4b117a811117cf7704809c84
DanielAmah <amahdanieljack@gmail.com>
2021-04-11 14:17:07 +0100

    migrate to JWT SECRET

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,17 +1,18 @@
 class JsonWebToken
+  JWT_SECRET = ENV["JWT_SECRET"]
   class << self
     def encode(payload, exp = 24.hours.from_now)
       # set token expiration time 
       payload[:exp] = exp.to_i
       
        # this encodes the user data(payload) with our secret key
-      token = JWT.encode(payload, Rails.application.secrets.secret_key_base)
+      token = JWT.encode(payload, JWT_SECRET)
       { token:token, exp: exp.to_i }
     end
 
     def decode(token)
       #decodes the token to get user data (payload)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+      body = JWT.decode(token, JWT_SECRET)[0]
       HashWithIndifferentAccess.new body
 
   
